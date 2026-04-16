ff165248eaf9604aabbba53c907e5ac78fe95469
NGH <25020179@vnu.edu.vn>
2026-04-17 01:00:47 +0700

    setup javaFX

---
+++ Auction_client/src/main/java/com/auction/client/MainApp.java
@@ -0,0 +1,28 @@
+package com.auction.client;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    int initialStageX = 600;
+    int initialStageY = 400;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/auction/client/views/login_view.fxml")); // load Login View as root node, with directory to login_view.fxml
+        Scene scene = new Scene(root, initialStageX, initialStageY); //create scene with root
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
