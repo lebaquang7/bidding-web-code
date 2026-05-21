6d150238433736b930a86511586d1468c79cbc9a
NGH <25020179@vnu.edu.vn>
2026-05-21 12:08:59 +0700

    feat(client): utilize gridpane for auction item display

--- Auction_client/src/main/java/com/auction/client/Controllers/MainMenuAuctionListPaneController.java
+++ Auction_client/src/main/java/com/auction/client/Controllers/MainMenuAuctionListPaneController.java
@@ -7,15 +7,21 @@ import java.util.List;
 import com.auction.shared.models.Item;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.GridPane;
 
 public class MainMenuAuctionListPaneController {
-    @FXML FlowPane mainMenuAuctionListFlowPane;
+    @FXML GridPane mainMenuAuctionListGridPane;
+    @FXML Pagination mainMenuAuctionListPagination;
     ObservableList<Item> itemList = FXCollections.observableArrayList();
+    //no. of items displayed per gridpane page and columns of grid
+    private static final int ITEMS_PER_PAGE = 6;
+    private static final int COLUMN_COUNT = 2;
 
     public void initialize(){
         //Placeholder item
@@ -26,20 +32,78 @@ public class MainMenuAuctionListPaneController {
         });
         itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
         });
+        //placeholder extra item for pagination testing. To be removed.
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        itemList.add(new Item("item", "desc", BigDecimal.valueOf(500000.0), BigDecimal.valueOf(9000000.0)) {
+        });
+        itemList.add(new Item("item2", "desc", BigDecimal.valueOf(200000.0), BigDecimal.valueOf(6000000.0)) {
+        });
+        itemList.add(new Item("item3", "desc", BigDecimal.valueOf(100000.0), BigDecimal.valueOf(300000.0)) {
+        });
+        //^^^^placeholder extra item for pagination testing. To be removed.
 
-        renderItem(itemList);
+        //calc page count
+        int pageCount = (int) Math.ceil(itemList.size() / ITEMS_PER_PAGE);
+        //safety checkfor 0 page count
+        mainMenuAuctionListPagination.setPageCount(pageCount == 0 ? 1 : pageCount);
+        //initial set
+        mainMenuAuctionListPagination.setCurrentPageIndex(0);
+        renderItem(itemList, 0);
+
+        //listen to change in page pagination
+        mainMenuAuctionListPagination.currentPageIndexProperty().addListener((observable, oldIndex, newIndex) -> {
+            renderItem(itemList, newIndex.intValue());
+        });
+        //listen to change in list eles
+        itemList.addListener((ListChangeListener<Item>) change -> {
+            renderItem(itemList, mainMenuAuctionListPagination.getCurrentPageIndex());
+        });
     }
 
     /**
      * render list of pane items
      * @param list
      */
-    public void renderItem(List<Item> list){
+    public void renderItem(List<Item> list, int paginationIndex){
         //clear old panes
-        mainMenuAuctionListFlowPane.getChildren().clear();
+        mainMenuAuctionListGridPane.getChildren().clear();
+        //calc index of list items to display
+        int startIndex = paginationIndex * ITEMS_PER_PAGE;
+        int endIndex = Math.min(paginationIndex * ITEMS_PER_PAGE + ITEMS_PER_PAGE, list.size());
 
-        for (Item item : list) {
+        List<Item> itemListSublist = list.subList(startIndex, endIndex);
+        for (int i = 0; i < itemListSublist.size(); i++) {
             try {
+                Item item = itemListSublist.get(i);
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/auction/client/views/mainMenu_auctionCard.fxml"));
               
                 //load data of the card
@@ -47,9 +111,10 @@ public class MainMenuAuctionListPaneController {
                 //get loader controller, set data to each item's data
                 AuctionCardController controller = loader.getController();
                 controller.setData(item);
-            
-                // add card to flowpane
-                mainMenuAuctionListFlowPane.getChildren().add(card);
+
+                int columnIndex = i/COLUMN_COUNT;
+                int rowIndex = i%COLUMN_COUNT;
+                mainMenuAuctionListGridPane.add(card, columnIndex, rowIndex);
             } catch (IOException e) {
                 e.printStackTrace();
             }
