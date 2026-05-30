eb4c9f4e3e75425ac2ba3268907cca78c8ddb79a
NGH <25020179@vnu.edu.vn>
2026-05-30 21:04:05 +0700

    feat(client): circle indicating auction status

--- Auction_client/src/main/java/com/auction/client/Controllers/AuctionCardController.java
+++ Auction_client/src/main/java/com/auction/client/Controllers/AuctionCardController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 
 public class AuctionCardController {
   @FXML
@@ -66,6 +67,8 @@ public class AuctionCardController {
                     }
                   });
             });
+
+    //TODO: initialize and listenner for updateColorByAuctionState method to update auction status in real time
   }
 
   public void mainMenuAuctionCardGoToItemDetails(ActionEvent event) {
@@ -76,5 +79,33 @@ public class AuctionCardController {
   public void mainMenuAuctionCardGoToAuction(ActionEvent event) {
     SceneController.switchToItemView(
         "/com/auction/client/views/auction_view.fxml", event, currentItem);
+  }
+
+  @FXML
+  private Circle mainMenuAuctionCardStatusCircle;
+
+  public void updateColorByAuctionState(String auctionState) {
+    String targetColor;
+
+    switch (auctionState.toUpperCase()) {
+      case "OPEN":
+        targetColor = "#47ff66"; // ?? change if theres difference between open and running status?
+        break;
+      case "RUNNING":
+        targetColor = "#47ff66";
+        break;
+      case "FINISHED":
+        targetColor = "#45cbf0";
+        break;
+      case "CANCELLED":
+        targetColor = "#f53535";
+        break;
+      case "PAID":
+        targetColor = "#db35f5";
+        break;
+      default:
+        targetColor = "#7f8c8d";
+    }
+    mainMenuAuctionCardStatusCircle.setStyle("-fx-auction-status-color: " + targetColor + ";");
   }
 }
